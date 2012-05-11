5e36ff416efddbc3a3c7534d54a5f589dac8c8db
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-10 23:11:52 -0500

    Problem 12 Optmization

--- 012.rb
+++ 012.rb
@@ -1,26 +1,20 @@
-def triangle(i)
-	sum = 0
-	(1..i).each { |j|
-	  sum += j
-	}
-	sum
-end
-
-def factors(i)
-  total = 2
-  (2..(i/2)).each { |j|
-    total += 1 if i % j == 0
-  }
-  total
-end
-
 counter = 1
 while true
-  num = triangle(counter)
-  factor = factors(num)
-  puts num.to_s + ": " + factor.to_s
-  break if factor > 500
+  
+  # Find next Triangle number
+  triangle = 0
+  (1..counter).each { |j|
+    triangle += j
+  }
+
+  # Factor Triangle number
+  factors = 2
+  (2..(Math.sqrt(triangle))).each { |i|
+    factors += 2 if triangle % i == 0
+  }
+    
+  break if factors > 500
   counter += 1
 end
 
-puts num
+puts triangle
