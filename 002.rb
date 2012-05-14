b388a169d8d02ef130beaab29a18d59d64558a26
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-14 10:09:34 -0500

    Problem 2 Refactor

--- 002.rb
+++ 002.rb
@@ -1,12 +1,6 @@
 def fibonnaci_sum(a,b)
   sum = a % 2 == 0 ? a : 0
-  if b >= 4000000
-    return sum
-  end
-  return sum + fibonnaci_sum(b,a+b)
+  return b >= 4000000 ? sum : sum + fibonnaci_sum(b,a+b)
 end
-
-a = 1
-b = 2
 
 puts fibonnaci_sum(1,2)
