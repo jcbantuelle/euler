049f37f5db1a2a12fe6389eb2e267d2eca3e7d43
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-15 21:45:47 -0500

    Problem 384 Updates

--- 384.rb
+++ 384.rb
@@ -3,7 +3,6 @@ seen = {}
 fibonacci[0] = 1
 fibonacci[1] = 1
 
-
 2.upto(45) do |i|
   fibonacci[i] = fibonacci[i-1] + fibonacci[i-2]
   seen[fibonacci[i]] = 0
@@ -18,7 +17,7 @@ substitution = {
 
 string = "1 1 1 -1"
 
-1.upto(23) do
+1.upto(24) do
   parsed = string.scan(/-?\d\s-?\d/)
   string = string + " " + parsed.slice(parsed.size/2..parsed.size).map{|i| substitution[i]}.join(" ")
 end
@@ -34,7 +33,11 @@ string.split(/\s/).each_with_index do |number, index|
   if seen.include?(sum)
     seen[sum] += 1
     if sum == t and seen[sum] == c
-      puts "g(#{t}, #{c}) = #{index}"
+      t_even = t % 2 == 0
+      c_even = c % 2 == 0
+      index_even = index % 2 == 0
+      puts "g(#{t_even ? "E" : "O"}, #{c_even ? "E" : "O"}) = #{index_even ? "E" : "O"}"
+      #puts "g(#{t}, #{c}) = #{index}"
       fibonacci_index += 1
       c = t
       t = fibonacci[fibonacci_index]
