f5ad0c580652a689678c599780d1a2deb618b3b8
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-17 00:13:50 -0500

    Problem 34 Update

--- 034.rb
+++ 034.rb
@@ -1,7 +1,7 @@
 factorials = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880]
 
 total = 0
-10.upto(999999) do |number|
+10.upto(99999) do |number|
   checksum = 0
   number.to_s.split(//).each{|digit| checksum += factorials[digit.to_i]}
   total += number if checksum == number
