ba86bc04d3a6c56690cb2dd299a2b47c46925d35
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-24 19:01:47 -0500

    Problem 32 Refactor

--- 032.rb
+++ 032.rb
@@ -4,7 +4,10 @@ sum = 0
   2.upto(Math.sqrt(product)) do |factor|
     if product % factor == 0
       factor_two = product / factor
-      if (product.to_s + factor.to_s + factor_two.to_s).split(//).sort.join('') == '123456789'
+      product_string = product.to_s + factor.to_s + factor_two.to_s
+      product_array = []
+      product_string.each_char{|char| product_array << char}
+      if product_array.sort == %w(1 2 3 4 5 6 7 8 9)
         sum += product
         break
       end
