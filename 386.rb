b181457b18c8c70b35ed843c229195d2e4fe7639
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-31 16:47:01 -0500

    Problem 386 updates (Still not working)

--- 386.rb
+++ 386.rb
@@ -8,26 +8,28 @@ file.each do |prime|
   primes_hash[prime] = true
 end
 
-sum = 1
-2.upto(100_000_000) do |n|
-  puts n if n % 100_000 == 0
-  if primes_hash.include?(n)
-    sum += 1
-    next
-  end
-  
-  factored = n
-  primes_array.each do |prime|
-    while factored % prime == 0
-      factored /= prime
-      sum += 1
-    end
-    if primes_hash.include?(factored)
-      sum += 1
-      break
-    end
-    break if factored == 1
-  end
-end
+puts primes_array.size
 
-puts sum
+#sum = 1
+#2.upto(100_000_000) do |n|
+#  puts n if n % 100_000 == 0
+#  if primes_hash.include?(n)
+#    sum += 1
+#    next
+#  end
+#  
+#  factored = n
+#  primes_array.each do |prime|
+#    while factored % prime == 0
+#      factored /= prime
+#      sum += 1
+#    end
+#    if primes_hash.include?(factored)
+#      sum += 1
+#      break
+#    end
+#    break if factored == 1
+#  end
+#end
+#
+#puts sum
