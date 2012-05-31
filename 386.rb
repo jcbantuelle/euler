20251008e9703c471e4693b131d7dfb612771ab0
Justin Bantuelle <justin@mudbugmedia.com>
2012-05-31 15:19:46 -0500

    Problem 386

---
+++ 386.rb
@@ -0,0 +1,33 @@
+primes_array = []
+primes_hash = {}
+
+file = IO.readlines('primes.txt')
+file.each do |prime|
+  prime = prime.to_i
+  primes_array << prime
+  primes_hash[prime] = true
+end
+
+sum = 1
+2.upto(100_000_000) do |n|
+  puts n if n % 100_000 == 0
+  if primes_hash.include?(n)
+    sum += 1
+    next
+  end
+  
+  factored = n
+  primes_array.each do |prime|
+    while factored % prime == 0
+      factored /= prime
+      sum += 1
+    end
+    if primes_hash.include?(factored)
+      sum += 1
+      break
+    end
+    break if factored == 1
+  end
+end
+
+puts sum
