68f174525b83ab38cc1de12f8a78d91bc565af73
Justin Bantuelle <justin@mudbugmedia.com>
2012-06-10 18:03:35 -0500

    Problem 59 Update

--- 059.rb
+++ 059.rb
@@ -4,15 +4,25 @@ file.each do |cipher|
   cipher_values = cipher.split(/,/).map{|number| number.to_i}
 end
 
-key1 = 103
-key2 = 111
-key3 = 100
+found = false
 
-decipher = Array.new(cipher_values)
-decipher.each_index do |index|
-  decipher[index] = decipher[index] ^ key1 if index % 3 == 0
-  decipher[index] = decipher[index] ^ key2 if index % 3 == 1
-  decipher[index] = decipher[index] ^ key3 if index % 3 == 2
+("a".ord).upto("z".ord) do |key1|
+  ("a".ord).upto("z".ord) do |key2|
+    ("a".ord).upto("z".ord) do |key3|
+      decipher = Array.new(cipher_values)
+      decipher.each_index do |index|
+        decipher[index] = decipher[index] ^ key1 if index % 3 == 0
+        decipher[index] = decipher[index] ^ key2 if index % 3 == 1
+        decipher[index] = decipher[index] ^ key3 if index % 3 == 2
+      end
+      decrypted = decipher.map{|letter| letter.chr}.join('')
+      if decrypted.include?(" the ")
+        puts decipher.inject(:+)
+        found = true
+      end
+      break if found
+    end
+    break if found
+  end
+  break if found
 end
-
-puts decipher.inject(:+)
