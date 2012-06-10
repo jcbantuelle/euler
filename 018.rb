90e0618a527a132141a10a3b309a70645bfa3660
Justin Bantuelle <justin@mudbugmedia.com>
2012-06-09 23:23:56 -0500

    Problem 18 Refactor

--- 018.rb
+++ 018.rb
@@ -1,18 +1,18 @@
 row = Array.new
 
 file = IO.readlines('triangle_18.txt')
-row_num = 1
+row_num = 0
 file.each do |record|
   row[row_num] = record.split(/\s/).map{|number| number.to_i}
   row_num += 1
 end
 
-14.downto(1) do |i|
+13.downto(0) do |i|
   row[i].each_with_index do |val, key|
     left = row[i+1][key]
     right = row[i+1][key+1]
-    row[i][key] += left > right ? left : right
+    row[i][key] += [left, right].max
   end
 end
 
-puts row[1][0]
+puts row[0][0]
