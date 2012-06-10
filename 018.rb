b8d1ad40788c4c7a6a6f3e21ed238a2602c3ef1c
Justin Bantuelle <justin@mudbugmedia.com>
2012-06-09 23:34:02 -0500

    Problem 18 Optimization

--- 018.rb
+++ 018.rb
@@ -8,7 +8,7 @@ file.each do |record|
 end
 
 13.downto(0) do |i|
-  row[i].each_with_index do |val, key|
+  row[i].each_index do |key|
     left = row[i+1][key]
     right = row[i+1][key+1]
     row[i][key] += [left, right].max
