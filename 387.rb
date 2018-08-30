32e851240f36fdb71ebb10672af4fa802254e0e3
Justin Bantuelle <justin@mudbugmedia.com>
2018-08-30 15:23:49 -0500

    Problem 387

--- 387.rb
+++ 387.rb
@@ -1,14 +1,36 @@
-primes_array = []
-primes_hash = {}
+require 'pp'
+require 'prime'
 
-file = IO.readlines('primes.txt')
-file.each do |prime|
-  prime = prime.to_i
-  break if prime > 10000
-  primes_array << prime
-  primes_hash[prime] = true
+primes = []
+
+class Integer
+  def sum_of_digits
+    self.to_s.chars.map(&:to_i).reduce(&:+)
+  end
+
+  def harshad_number?
+    self % sum_of_digits == 0
+  end
 end
 
-primes_array.each do |prime|
-  
+previous_harshad_iteration = (10..99).to_a.select(&:harshad_number?)
+
+power_of_ten = 14
+
+start_time = Time.now
+(power_of_ten-2).times do |exponent|
+  next_harshad_iteration = []
+  previous_harshad_iteration.each do |harshad_number|
+    is_strong = Prime.prime?(harshad_number/harshad_number.sum_of_digits)
+    new_h = harshad_number*10
+    0.upto(9) do |i|
+      h = new_h + i
+      next_harshad_iteration << h if h.harshad_number?
+      primes << h if is_strong && Prime.prime?(h)
+    end
+  end
+  previous_harshad_iteration = next_harshad_iteration
 end
+pp "Found in #{Time.now - start_time} seconds"
+
+pp primes.inject(&:+)
