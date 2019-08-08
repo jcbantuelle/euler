11071ff81656184ed7d471c8fc2b5370b37e7aed
Justin Bantuelle <justin@mudbugmedia.com>
2019-08-08 10:23:48 -0500

    Fix Problem 66

--- 066.rb
+++ 066.rb
@@ -1,25 +1,43 @@
-def solvable_for_y(x, d)
-  y = Math.sqrt((x * x - 1.0) / d)
-  is_integer?(y)
+require 'pp'
+
+def valid_equation?(x, y, d)
+  x**2 - y**2 * d == 1
 end
 
-def is_integer?(num)
-  num == num.floor
+def fundamental_solution(d)
+  a = Math.sqrt(d)
+  a = a.floor
+  num = 1
+  b = a
+  h = [0, 1]
+  k = [1, 0]
+
+  count = 2
+  while count == 2 || !valid_equation?(h.last, k.last, d) do
+    h << a * h[count-1] + h[count-2]
+    k << a * k[count-1] + k[count-2]
+
+    a = (num / (Math.sqrt(d) - b)).floor
+    num = ((d - b**2) / num).floor
+    b = -(b - num * a)
+    count += 1
+  end
+  h.last
 end
 
-largest_x = 0
-largest_d = 0
+largest = {
+  d: 0,
+  answer: 0
+}
+
 2.upto(1000) do |d|
-  next if is_integer?(Math.sqrt(d))
-  x = 2
-  while !solvable_for_y(x, d) do
-    x += 1
-  end
-  if x > largest_x
-    largest_x = x
-    largest_d = d
+  next if Math.sqrt(d) % 1 == 0
+  answer = fundamental_solution(d)
+  if answer > largest[:answer]
+    largest = {
+      d: d,
+      answer: answer
+    }
   end
 end
-
-puts largest_x
-puts largest_d
+pp largest
